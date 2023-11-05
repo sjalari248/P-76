eaada184fa40dbbf3f4183fb70764c57e08a5967
sjalari248 <81190213+sjalari248@users.noreply.github.com>
2023-11-05 11:11:54 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,82 @@
+function mercury()
+{
+    window.location="mercury.html";
+}
+
+function venus()
+{
+    window.location="venus.html";
+}
+
+function earth()
+{
+    window.location="earth.html";
+}
+
+function mars()
+{
+    window.location="mars.html";
+}
+
+function jupitor()
+{
+    window.location="jupitor.html";
+}
+
+function saturn()
+{
+    window.location="saturn.html";
+}
+
+function uranus()
+{
+    window.location="uranus.html";
+}
+
+function neptune()
+{
+    window.location="neptune.html";
+}
+
+
+
+
+function mercuryBack()
+{
+    window.location="index.html";
+}
+
+function venusBack()
+{
+    window.location="index.html";
+}
+
+function earthBack()
+{
+    window.location="index.html";
+}
+
+function marsBack()
+{
+    window.location="index.html";
+}
+
+function jupitorBack()
+{
+    window.location="index.html";
+}
+
+function saturnBack()
+{
+    window.location="index.html";
+}
+
+function uranusBack()
+{
+    window.location="index.html";
+}
+
+function neptuneBack()
+{
+    window.location="index.html";
+}
